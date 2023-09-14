1b92b887b5fd9fc87fb438a391eb4e766fb6ca04
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-09-14 16:27:29 +0500

    fix: Fixed paths to images for vercel hosting

--- src/dataSlider.jsx
+++ src/dataSlider.jsx
@@ -1,28 +1,28 @@
 export const dataSlider = [
     {
         key: 1,
-        src: './img/01_NF.jpg',
+        src: '/src/img/01_NF.jpg',
         alt: 'NF - The Search',
         title: 'NF - The Search',
         width: 400,
     }, 
     {
         key: 2,
-        src: './img/02_TheBeatles.jpg',
+        src: '/src/img/02_TheBeatles.jpg',
         alt: 'The Beatles - Abbey Road',
         title: 'The Beatles - Abbey Road',
         width: 400,
     }, 
     {
         key: 3,
-        src: './img/03_PinkFloyd.jpg',
+        src: '/src/img/03_PinkFloyd.jpg',
         alt: 'Pink Floyd - Dark Side of the Moon',
         title: 'Pink Floyd - Dark Side of the Moon',
         width: 400,
     }, 
     {
         key: 4,
-        src: './img/04_Metallica.jpg',
+        src: '/src/img/04_Metallica.jpg',
         alt: 'Metallica - Master of Puppets',
         title: 'Metallica - Master of Puppets',
         width: 400,
