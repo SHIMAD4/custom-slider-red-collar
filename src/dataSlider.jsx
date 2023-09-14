8862bf813a76223e67aff7aba5fc4e8036c1e7b4
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-09-14 16:15:59 +0500

    fix: Fixed dataSlider for vercel hosting

--- src/dataSlider.jsx
+++ src/dataSlider.jsx
@@ -1,4 +1,4 @@
-const dataSlider = [
+export const dataSlider = [
     {
         key: 1,
         src: '../public/img/01_NF.jpg',
@@ -28,5 +28,3 @@ const dataSlider = [
         width: 400,
     }
 ]
-
-export default dataSlider
